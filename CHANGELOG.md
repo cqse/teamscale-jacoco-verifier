57f08611ff06a8674c0f223efe1743e1daadb3d3
Fabian Streitel <streitel@cqse.eu>
2018-11-13 17:42:52 +0100

    fix default package name in UI

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+We use [semantic versioning][semver]
+
+# Next version
+
+# 1.0.1
+
+- [bugfix] UI used to display invalid package name by default
+
+# 1.0.0
+
+- initial release!
+
+
+[semver]: http://semver.org/
--- src/main/java/MainForm.java
+++ src/main/java/MainForm.java
@@ -1,3 +1,6 @@
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
@@ -53,30 +56,26 @@ public class MainForm {
         selectButton.setText("Select...");
         root.add(selectButton, cc.xy(5, 1));
         packageText = new JTextField();
-        packageText.setText("e.g: com.yourcompany");
+        packageText.setText("com.yourcompany");
         packageText.setToolTipText("e.g: com.yourcompany");
         root.add(packageText, cc.xyw(3, 3, 3, CellConstraints.FILL, CellConstraints.DEFAULT));
         final JPanel panel1 = new JPanel();
-        panel1.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.setLayout(new GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
         root.add(panel1, cc.xyw(1, 5, 5));
-        final com.intellij.uiDesigner.core.Spacer spacer1 = new com.intellij.uiDesigner.core.Spacer();
-        panel1.add(spacer1, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1,
-                com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER,
-                com.intellij.uiDesigner.core.GridConstraints.FILL_VERTICAL, 1,
-                com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        panel1.add(spacer1,
+                new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1,
+                        GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         verifyCoverageFileButton = new JButton();
         verifyCoverageFileButton.setText("Verify Coverage File");
-        panel1.add(verifyCoverageFileButton, new com.intellij.uiDesigner.core.GridConstraints(0, 2, 1, 1,
-                com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER,
-                com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL,
-                com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK |
-                        com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW,
-                com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final com.intellij.uiDesigner.core.Spacer spacer2 = new com.intellij.uiDesigner.core.Spacer();
-        panel1.add(spacer2, new com.intellij.uiDesigner.core.GridConstraints(0, 1, 1, 1,
-                com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER,
-                com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL,
-                com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        panel1.add(verifyCoverageFileButton,
+                new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                        GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+                        GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        panel1.add(spacer2,
+                new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
+                        GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         resultLabel = new JLabel();
         resultLabel.setText("Result");
         root.add(resultLabel, cc.xyw(1, 7, 5, CellConstraints.DEFAULT, CellConstraints.TOP));
