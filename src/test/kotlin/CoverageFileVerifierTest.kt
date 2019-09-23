5d41f0d9d8a5850771eb1f4f9e043a002b9649c0
Andreas Goeb <goeb@cqse.eu>
2019-09-23 11:08:41 +0200

    Fix test

--- src/test/kotlin/CoverageFileVerifierTest.kt
+++ src/test/kotlin/CoverageFileVerifierTest.kt
@@ -7,7 +7,7 @@ class CoverageFileVerifierTest {
     fun `valid file with coverage in package`() {
         val result = CoverageFileVerifier.verify("""
             <?xml version="1.0" encoding="UTF-8" standalone="yes"?><report name="test"><group name="coverageTest"><package name="com/mypackage"><class name="com/acme/Customer"><method name="&lt;init&gt;" desc="()V" line="3"><counter type="INSTRUCTION" missed="3" covered="3"/></method></class></package></group></report>
-        """.trimIndent(), "com.mypackage")
+        """.trimIndent().byteInputStream(), "com.mypackage")
         assertk.assert(result).isEqualTo(VerificationResult.SUCCESS)
     }
 
@@ -15,7 +15,7 @@ class CoverageFileVerifierTest {
     fun `valid file with doctype`() {
         val result = CoverageFileVerifier.verify("""
             <?xml version="1.0" encoding="UTF-8" standalone="yes"?><!DOCTYPE report PUBLIC "-//JACOCO//DTD Report 1.0//EN" "report.dtd"><report name="test"><group name="coverageTest"><package name="com/mypackage"><class name="com/acme/Customer"><method name="&lt;init&gt;" desc="()V" line="3"><counter type="INSTRUCTION" missed="3" covered="3"/></method></class></package></group></report>
-        """.trimIndent(), "com.mypackage")
+        """.trimIndent().byteInputStream(), "com.mypackage")
         assertk.assert(result).isEqualTo(VerificationResult.SUCCESS)
     }
 
@@ -23,7 +23,7 @@ class CoverageFileVerifierTest {
     fun `package not in report`() {
         val result = CoverageFileVerifier.verify("""
             <?xml version="1.0" encoding="UTF-8" standalone="yes"?><report name="test"><group name="coverageTest"></group><counter type="INSTRUCTION" missed="3" covered="0"/></report>
-        """.trimIndent(), "com.mypackage")
+        """.trimIndent().byteInputStream(), "com.mypackage")
         assertk.assert(result).isEqualTo(VerificationResult.PACKAGE_NOT_IN_REPORT)
     }
 
@@ -31,7 +31,7 @@ class CoverageFileVerifierTest {
     fun `no coverage at all`() {
         val result = CoverageFileVerifier.verify("""
             <?xml version="1.0" encoding="UTF-8" standalone="yes"?><report name="test"><group name="coverageTest"><package name="com/mypackage"><class name="com/acme/Customer"><method name="&lt;init&gt;" desc="()V" line="3"><counter type="INSTRUCTION" missed="3" covered="0"/></method></class></package></group></report>
-        """.trimIndent(), "com.mypackage")
+        """.trimIndent().byteInputStream(), "com.mypackage")
         assertk.assert(result).isEqualTo(VerificationResult.NO_COVERAGE_AT_ALL)
     }
 
@@ -39,13 +39,13 @@ class CoverageFileVerifierTest {
     fun `no coverage in package`() {
         val result = CoverageFileVerifier.verify("""
             <?xml version="1.0" encoding="UTF-8" standalone="yes"?><report name="test"><group name="coverageTest"><package name="com/mypackage"><class name="com/acme/Customer"><method name="&lt;init&gt;" desc="()V" line="3"><counter type="INSTRUCTION" missed="3" covered="0"/></method></class></package><package name="com/other"><class name="com/acme/Customer"><method name="&lt;init&gt;" desc="()V" line="3"><counter type="INSTRUCTION" missed="3" covered="1"/></method></class></package></group></report>
-        """.trimIndent(), "com.mypackage")
+        """.trimIndent().byteInputStream(), "com.mypackage")
         assertk.assert(result).isEqualTo(VerificationResult.NO_COVERAGE_IN_PACKAGE)
     }
 
     @Test
     fun `totally invalid file`() {
-        val result = CoverageFileVerifier.verify("invalid file content", "com.mypackage")
+        val result = CoverageFileVerifier.verify("invalid file content".byteInputStream(), "com.mypackage")
         assertk.assert(result).isEqualTo(VerificationResult.NOT_A_JACOCO_REPORT)
     }
 
